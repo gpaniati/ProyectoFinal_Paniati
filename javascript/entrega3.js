44bedf71aca6dc7459563993527eaa170a416f57
gpaniati <gpaniati@gmail.com>
2022-10-27 21:33:59 -0300

    Capturo campos de pantalla

--- javascript/entrega3.js
+++ javascript/entrega3.js
@@ -1,2 +1,19 @@
 const carritoHabitaciones = [];
 const carritoServicios = [];
+
+//Tomo control de los campos de busqueda.
+let fechaEntrada = document.getElementById("inputFechaEntrada");
+let fechaSalida = document.getElementById("inputFechaSalida");
+let comboHuespedes = document.getElementById("inputHuespedes");
+
+//Tomo control del boton de consulta y asigno evento.
+let botonConsulta = document.getElementById("botonConsulta");
+botonConsulta.addEventListener("click", filtrarBusqueda);
+
+function filtrarBusqueda(){
+    console.log("Idiota");
+    console.log(fechaEntrada.value);
+    //console.log(fechaSalida.text);
+    let qHuespedes = comboHuespedes.options[comboHuespedes.selectedIndex].value;
+    console.log(qHuespedes);
+}
