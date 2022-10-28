abdb4ad71498268a15968d37b0978e7844bf3837
gpaniati <gpaniati@gmail.com>
2022-10-28 07:56:25 -0300

    Capturar fechas y sacar dias entre ellas

--- javascript/entrega3.js
+++ javascript/entrega3.js
@@ -2,18 +2,50 @@ const carritoHabitaciones = [];
 const carritoServicios = [];
 
 //Tomo control de los campos de busqueda.
-let fechaEntrada = document.getElementById("inputFechaEntrada");
-let fechaSalida = document.getElementById("inputFechaSalida");
+let comboFechaIngreso = document.getElementById("inputFechaIngreso");
+let comboFechaSalida = document.getElementById("inputFechaSalida");
 let comboHuespedes = document.getElementById("inputHuespedes");
+//Inicializo los combos de fechas con la fecha del dia.
+comboFechaIngreso.value = (obtenerFechaActual())[0];
+comboFechaSalida.value = (obtenerFechaActual())[0];
 
 //Tomo control del boton de consulta y asigno evento.
 let botonConsulta = document.getElementById("botonConsulta");
 botonConsulta.addEventListener("click", filtrarBusqueda);
 
-function filtrarBusqueda(){
-    console.log("Idiota");
-    console.log(fechaEntrada.value);
-    //console.log(fechaSalida.text);
+function filtrarBusqueda() {
+    let fechaIngreso = new Date(comboFechaIngreso.value);
+    let fechaSalida = new Date(comboFechaSalida.value);
     let qHuespedes = comboHuespedes.options[comboHuespedes.selectedIndex].value;
-    console.log(qHuespedes);
+    let qDiasHospedaje = calcularDias(fechaIngreso, fechaSalida);
+}
+
+//Obtengo fecha actual.
+function obtenerFechaActual(){
+    let fActual = new Date();
+    let dia = fActual.getDate();
+    let mes = fActual.getMonth() + 1;
+    let anio = fActual.getFullYear();
+    //Formato AAAA-MM-DD
+    let fActualFormato1 = `${anio}-${mes}-${dia}`;
+    //Formato DD/MM/AAAA
+    let fActualFormato2 = `${dia}/${mes}/${anio}`;
+    return [fActualFormato1,fActualFormato2];
+}
+
+//Valida las fechas y calcula la cantidad de días entre ellas.
+function calcularDias(fIngreso, fSalida) {
+    let fActual = (obtenerFechaActual())[0];
+    let dias = 0;
+    if (fIngreso < fActual){
+        alert("La fecha de entrada debe ser mayor a la fecha de HOY!!!");
+    }else {
+        if (fIngreso >= fSalida){
+            alert("La fecha de salida debe ser mayor a la fecha de entrada!!!");
+        }else{
+            let difFechas = fSalida.getTime() - fIngreso.getTime();
+            dias = Math.round(difFechas / (1000 * 60 * 60 * 24));
+        }
+    }
+    return dias;
 }
