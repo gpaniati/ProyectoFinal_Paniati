4b39e444e0d0166c0b200355424dc390ab4cc678
gpaniati <gpaniati@gmail.com>
2022-10-30 19:20:54 -0300

    Agrego servicios al la lista

--- javascript/entrega3.js
+++ javascript/entrega3.js
@@ -18,17 +18,21 @@ botonConsulta.addEventListener("click", filtrarBusqueda);
 //Filtro array de habiataciones segun condiciones de búsqueda.
 function filtrarBusqueda() {
     //Limpio carrito habitaciones.
-    carritoHabitaciones.splice(0,carritoHabitaciones.length)
+    carritoHabitaciones.splice(0, carritoHabitaciones.length)
+    carritoServicios.splice(0, carritoServicios.length)
     //Limpio errores y avisos.
     mensajeError.innerText = ("");
     mensajeAviso.innerText = ("");
+    //Limpio tabla
+    let cuerpotabla = document.getElementById("tablaBody");
+    cuerpotabla.innerHTML = ``;
     //Obtengo parámetros de busqueda.
     let fechaIngreso = new Date(comboFechaIngreso.value);
     let fechaSalida = new Date(comboFechaSalida.value);
     let qHuespedes = comboHuespedes.options[comboHuespedes.selectedIndex].value;
     let qDiasHospedaje = calcularDias(fechaIngreso, fechaSalida);
     //Filtro las habitaciones a mostrar de acuerdo a la cantidad de huespedes.
-    if (qDiasHospedaje > 0){
+    if (qDiasHospedaje > 0) {
         let habitacionesDisponibles = habitaciones.filter((habitacion) => ((habitacion.capacidad >= qHuespedes) && (habitacion.estaOcupada() == false)));
         mostrarHabitaciones(habitacionesDisponibles);
         definirEventosHabitaciones(habitacionesDisponibles);
@@ -38,11 +42,11 @@ function filtrarBusqueda() {
 
 //HABITACIONES.
 //Muestra la grillas de habitaciones filtradas.
-function mostrarHabitaciones(habitacionesDisponibles){
+function mostrarHabitaciones(habitacionesDisponibles) {
     let cartaHabitaciones = document.getElementById("cardHabitaciones");
     cartaHabitaciones.innerHTML = ``;
     cardServicios.innerHTML = ``;
-    for (const habitacion of habitacionesDisponibles){
+    for (const habitacion of habitacionesDisponibles) {
         let cartaDinamica = document.createElement("div");
         cartaDinamica.innerHTML = `
             <img src="${habitacion.imagenHabitacion}" class="card-img-top" alt="${habitacion.nombreHabitacion}">
@@ -54,14 +58,14 @@ function mostrarHabitaciones(habitacionesDisponibles){
             </div>
         `;
         cartaHabitaciones.append(cartaDinamica);
-        cartaDinamica.className="cartaDinamica col-lg-2 col-md-3 col-sm-4 col-6";
+        cartaDinamica.className = "cartaDinamica col-lg-2 col-md-3 col-sm-4 col-6";
     }
 }
 
 //Funcion para definir eventos de todos los botones de habitaciones a seleccionar.
-function definirEventosHabitaciones(habitacionesDisponibles){
+function definirEventosHabitaciones(habitacionesDisponibles) {
     habitacionesDisponibles.forEach((habitacion) => {
-        document.getElementById(`botonAgregarHabitacion${habitacion.idHabitacion}`).addEventListener("click",function(){
+        document.getElementById(`botonAgregarHabitacion${habitacion.idHabitacion}`).addEventListener("click", function () {
             agregarACarritoDeHabitaciones(habitacion)
         })
     })
@@ -69,10 +73,10 @@ function definirEventosHabitaciones(habitacionesDisponibles){
 
 //SERVICIOS.
 //Muestra la grilla de servicios.
-function mostrarServicios(serviciosDisponibles){
+function mostrarServicios(serviciosDisponibles) {
     let cartaServicios = document.getElementById("cardServicios");
     cartaServicios.innerHTML = ``;
-    for (const servicio of serviciosDisponibles){
+    for (const servicio of serviciosDisponibles) {
         let cartaDinamica = document.createElement("div");
         cartaDinamica.innerHTML = `
             <img src="${servicio.imagenServicio}" class="card-img-top" alt="${servicio.nombreServicio}">
@@ -84,26 +88,26 @@ function mostrarServicios(serviciosDisponibles){
             </div>
         `;
         cartaServicios.append(cartaDinamica);
-        cartaDinamica.className="cartaDinamica col-lg-3 col-md-4 col-sm-6 col-12";
+        cartaDinamica.className = "cartaDinamica col-lg-3 col-md-4 col-sm-6 col-12";
     }
 }
 
 //Funcion para definir eventos de todos los botones de servicios a seleccionar.
-function definirEventosServicios(serviciosDisponibles){
+function definirEventosServicios(serviciosDisponibles) {
     serviciosDisponibles.forEach((servicio) => {
-        document.getElementById(`botonAgregarServicio${servicio.idServicio}`).addEventListener("click",function(){
+        document.getElementById(`botonAgregarServicio${servicio.idServicio}`).addEventListener("click", function () {
             agregarACarritoDeServicios(servicio)
         })
     })
 }
 //AGREGAR A CARRITOS.
 function agregarACarritoDeHabitaciones(habitacion) {
-    if (carritoHabitaciones.length == 1){
+    if (carritoHabitaciones.length == 1) {
         mensajeError.innerText = ("Solo se puede seleccionar una habitación...");
-    }else{
+    } else {
         carritoHabitaciones.push(habitacion);
         agregarHabitacionALaLista(habitacion);
-        console.table(carritoHabitaciones);
+        //console.table(carritoHabitaciones);
         //Limpio seccion de cartas de habitaciones.
         let cartaHabitaciones = document.getElementById("cardHabitaciones");
         cartaHabitaciones.innerHTML = ``;
@@ -118,32 +122,31 @@ function agregarACarritoDeServicios(servicioElegido) {
     let servicioYaElegido = false;
     //Busco si el servicio ya fue seleccionado en el carrito de servicios.
     servicioYaElegido = carritoServicios.some((servicio) => servicio.idServicio == servicioElegido.idServicio);
-    if (servicioYaElegido == false){
+    if (servicioYaElegido == false) {
         carritoServicios.push(servicioElegido);
         agregarServicioALaLista(servicioElegido);
-        console.table(carritoServicios);
+        //console.table(carritoServicios);
         alert("SERVICIO AÑADIDO CORRECTAMENTE, Continue eligiendo o finalice su reserva...")
         //mensajeAviso.innerText = ("");
         mensajeError.innerText = ("");
-    }else{
+    } else {
         mensajeAviso.innerText = ("");
-        mensajeError.innerText = ("Servicio ya elegido; seleccione otro o finalice su reserva" );
+        mensajeError.innerText = ("Servicio ya elegido; seleccione otro o finalice su reserva");
     }
 }
 
 //FUNCIONES GENERALES
-
-function agregarHabitacionALaLista(habitacion){
+function agregarHabitacionALaLista(habitacion) {
     let cuerpotabla = document.getElementById("tablaBody");
     let qHuespedes = comboHuespedes.options[comboHuespedes.selectedIndex].value;
     let fechaIngreso = new Date(comboFechaIngreso.value);
     let fechaSalida = new Date(comboFechaSalida.value);
     let qDiasHospedaje = calcularDias(fechaIngreso, fechaSalida);
-    let precio = habitacion.calcularPrecioHabitacion(qHuespedes,qDiasHospedaje);
+    let precio = habitacion.calcularPrecioHabitacion(qHuespedes, qDiasHospedaje);
     cuerpotabla.innerHTML += `
         <tr>
-            <th class="imagenTabla"><img src="${habitacion.imagenHabitacion}" width=60px height=100px alt="${habitacion.nombreHabitacion}"></th>
-            <td>${habitacion.nombreHabitacion} - $R ${habitacion.precioPorPersona}</td>
+            <th class="imagenTabla"><img src="${habitacion.imagenHabitacion}" width=100px height=100px alt="${habitacion.nombreHabitacion}"></th>
+            <td>${habitacion.nombreHabitacion} / $R ${habitacion.precioPorPersona}</td>
             <td>${qHuespedes}</td>
             <td>${qDiasHospedaje}</td>
             <td>R$ ${precio}</td>
@@ -151,11 +154,26 @@ function agregarHabitacionALaLista(habitacion){
     `
 }
 
-function agregarServicioALaLista(servicio){
-
+function agregarServicioALaLista(servicio) {
+    let cuerpotabla = document.getElementById("tablaBody");
+    let qHuespedes = comboHuespedes.options[comboHuespedes.selectedIndex].value;
+    let fechaIngreso = new Date(comboFechaIngreso.value);
+    let fechaSalida = new Date(comboFechaSalida.value);
+    let qDiasHospedaje = calcularDias(fechaIngreso, fechaSalida);
+    let precio = servicio.calcularPrecioServicio(qDiasHospedaje);
+    cuerpotabla.innerHTML += `
+        <tr>
+            <th class="imagenTabla"><img src="${servicio.imagenServicio}" width=100px height=100px alt="${servicio.nombreHabitacion}"></th>
+            <td>${servicio.nombreServicio} / $R ${servicio.precioPorDia}</td>
+            <td></td>
+            <td>${qDiasHospedaje}</td>
+            <td>R$ ${precio}</td>
+        </tr>
+    `
 }
+
 //Obtengo fecha actual.
-function obtenerFechaActual(){
+function obtenerFechaActual() {
     let fActual = new Date();
     let dia = fActual.getDate();
     let mes = fActual.getMonth() + 1;
@@ -164,19 +182,19 @@ function obtenerFechaActual(){
     let fActualFormato1 = `${anio}-${mes}-${dia}`;
     //Formato DD/MM/AAAA
     let fActualFormato2 = `${dia}/${mes}/${anio}`;
-    return [fActualFormato1,fActualFormato2,fActual];
+    return [fActualFormato1, fActualFormato2, fActual];
 }
 
 //Valida las fechas y calcula la cantidad de días entre ellas.
 function calcularDias(fIngreso, fSalida) {
     let fActual = (obtenerFechaActual())[2];
     let dias = 0;
-    if (fIngreso < fActual){
+    if (fIngreso < fActual) {
         mensajeError.innerText = ("La fecha de entrada debe ser mayor a la fecha de HOY!!!");
-    }else {
-        if (fIngreso >= fSalida){
+    } else {
+        if (fIngreso >= fSalida) {
             mensajeError.innerText = ("La fecha de salida debe ser mayor a la fecha de entrada!!!");
-        }else{
+        } else {
             let difFechas = fSalida.getTime() - fIngreso.getTime();
             dias = Math.round(difFechas / (1000 * 60 * 60 * 24));
             mensajeError.innerText = ("");
