ded9069d6f607b76bb15220841d281bc88b7d13e
gpaniati <gpaniati@gmail.com>
2022-10-30 14:45:48 -0300

    Corrijo cartas de habiataciones

--- javascript/entrega3.js
+++ javascript/entrega3.js
@@ -5,6 +5,7 @@ const carritoServicios = [];
 let comboFechaIngreso = document.getElementById("inputFechaIngreso");
 let comboFechaSalida = document.getElementById("inputFechaSalida");
 let comboHuespedes = document.getElementById("inputHuespedes");
+let mensajeError = document.getElementById("mensajeError")
 //Inicializo los combos de fechas con la fecha del dia.
 comboFechaIngreso.value = (obtenerFechaActual())[0];
 comboFechaSalida.value = (obtenerFechaActual())[0];
@@ -27,24 +28,33 @@ function filtrarBusqueda() {
 }
 
 function mostrarHabitaciones(habitacionesDisponibles){
-    let seccionHabitaciones = document.getElementById("habitaciones");
-    seccionHabitaciones.innerHTML = ``;
+    let cartaHabitaciones = document.getElementById("cardHabitaciones");
+    cartaHabitaciones.innerHTML = ``;
     for (const habitacion of habitacionesDisponibles){
         let cartaDinamica = document.createElement("div");
-
         cartaDinamica.innerHTML = `
             <img src="${habitacion.imagenHabitacion}" class="card-img-top" alt="${habitacion.nombreHabitacion}">
             <div class="card-body">
                 <h4 class="card-title">${habitacion.nombreHabitacion}</h4>
-                <p class="card-text">Precio por persona: R$ ${habitacion.precioPorPersona}</p>
-                <a href="#" class="btn btn-primary boton">Agregar</a>
+                <p class="card-text">Preço por pessoa: R$ ${habitacion.precioPorPersona}</p>
+                <a href="#" class="btn btn-primary boton botonAgregar">Agregar</a>
             </div>
         `;
-        seccionHabitaciones.append(cartaDinamica);
-        cartaDinamica.className="card cartaDinamica col-lg-2";
+        cartaHabitaciones.append(cartaDinamica);
+        cartaDinamica.className="cartaDinamica col-lg-2 col-md-3 col-sm-4 col-6";
     }
 }
 
+//AGREGAR A CARRITOS.
+function agregarACarritoDeHabitaciones(habitacion) {
+    carritoHabitaciones.push(habitacion);
+    console.table(carritoHabitaciones);
+}
+
+function agregarACarritoDeServicios(servicio) {
+    carritoServicios.push(servicio);
+    console.table(carritoServicios);
+}
 
 
 //FUNCIONES GENERALES
@@ -59,21 +69,22 @@ function obtenerFechaActual(){
     let fActualFormato1 = `${anio}-${mes}-${dia}`;
     //Formato DD/MM/AAAA
     let fActualFormato2 = `${dia}/${mes}/${anio}`;
-    return [fActualFormato1,fActualFormato2];
+    return [fActualFormato1,fActualFormato2,fActual];
 }
 
 //Valida las fechas y calcula la cantidad de días entre ellas.
 function calcularDias(fIngreso, fSalida) {
-    let fActual = (obtenerFechaActual())[0];
+    let fActual = (obtenerFechaActual())[2];
     let dias = 0;
     if (fIngreso < fActual){
-        alert("La fecha de entrada debe ser mayor a la fecha de HOY!!!");
+        mensajeError.innerText = ("La fecha de entrada debe ser mayor a la fecha de HOY!!!");
     }else {
         if (fIngreso >= fSalida){
-            alert("La fecha de salida debe ser mayor a la fecha de entrada!!!");
+            mensajeError.innerText = ("La fecha de salida debe ser mayor a la fecha de entrada!!!");
         }else{
             let difFechas = fSalida.getTime() - fIngreso.getTime();
             dias = Math.round(difFechas / (1000 * 60 * 60 * 24));
+            mensajeError.innerText = ("");
         }
     }
     return dias;
